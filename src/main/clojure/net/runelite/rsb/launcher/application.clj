e52e26abec6e4efd978909f2c20fe22c3b8a468e
G <mortoxia@gmail.com>
2025-02-07 07:26:22 -0600

    zaza up the stuff

--- src/main/clojure/net/runelite/rsb/launcher/application.clj
+++ src/main/clojure/net/runelite/rsb/launcher/application.clj
@@ -1,46 +1,43 @@
 (ns net.runelite.rsb.launcher.application
-  (:require [clojure.core.async :as async :refer [chan go <! >!! close!]]
+  (:require [clojure.core.async :as async :refer [chan go <! <!! >!! close!]]
             [clojure.string :as str]
             [net.runelite.rsb.launcher.botlite :as botlite])
-  (:import [net.runelite.rsb.botLauncher BotLiteInterface]
-           [net.runelite.rsb.botLauncher BotClassLoader]
-           #_[net.runelite.rsb.botLauncher BotLite]
-           ))
+  (:import            #_[net.runelite.rsb.botLauncher BotLite]))
 
-(def bots ^BotLiteInterface [])
+(def bots [])
 
-(defn get-classloader 
+(defn get-classloader
   "Gets the classloader of an object."
   [obj]
   ((.getClassLoader (.getClass obj))))
 
-(defn get-bot 
+(defn get-bot
   "Retrieves the Bot for any object loaded in its client."
   [obj]
   (doseq [bot bots]
     (when (== (get-classloader obj) (get-classloader bot))
-      bot))
-  )
+      bot)))
 
-(defn add-bot 
+(defn add-bot
   ""
-  [headless]
-  (let [bot (botlite/init)] 
-    (.launch bot (into-array String ["--bot-runelite" "--developer-mode"]))
-    (conj bots bot)
-    ))
+  [show-ui]
+  (let [bot (botlite/create-botlite-instance show-ui)]
+    (println bot)
+    (println "Test")
+    #_(.launch bot (into-array String ["--bot-runelite" "--developer-mode"]))
+    #_(conj bots bot)))
 
 (defn command-line-listener
   "Handles command-line interfacing with the client.
    The REPL is likely to be more immediately useful in most cases."
   []
-  (go (loop [command (str/trim (read-line))] 
+  (go (loop [command (str/trim (read-line))]
         (case (first (str/split (str/lower-case command) #" "))
           "runscript" (println "runscript")
           "stopscript" (println "stopscript")
-          "addbot" (do 
+          "addbot" (do
                      (println "Adding bot")
-                     (add-bot false))
+                     (add-bot true))
           "checkstate" (println "checkstate")
           (println "Invalid command"))
         (recur (str/trim (read-line))))))
@@ -53,7 +50,7 @@
   (cond
     (contains? args "--bot-runelite")
     (do
-      (add-bot false)
+      (add-bot true)
       (command-line-listener))
     (contains? args "--salsa-bowl")
     (println "Random Behavior")
